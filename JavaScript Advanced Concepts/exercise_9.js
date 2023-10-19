78439fd7f0f4d7f2fda6dba1d03147e2069fabad
CantosCDenise <denisecantos2905@gmail.com>
2023-10-19 11:36:09 +0200

    exercise 9 done

---
+++ JavaScript Advanced Concepts/exercise_9.js
@@ -0,0 +1,13 @@
+const person = {
+    firstName: 'Mario',
+    lastName: 'Rossi',
+    age: 25
+  }
+
+  // Print values of person using Object.keys 
+  
+  console.log(person.firstName);
+  console.log(person.lastName);
+  console.log(person.age);
+
+ 
